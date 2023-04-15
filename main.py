3f7d1816cfc910e0382110194322cdebd5c22c46
Harshmehta14 <harshymehta@gmail.com>
2023-04-15 22:34:40 +0530

    Database setup

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from model import *
+import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+os.path.join(os.path.abspath(os.path.dirname(__file__)),"database.sqlite3")
+db.init_app(app)
+with app.app_context():
+    print("database created")
+    db.create_all()
+
 
 @app.route("/")
 def home():
-    return  render_template('dashboard.html')
+    users =  User.query.all()
+    print(users)
+    return  render_template('dashboard.html', users=users)
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
-    return  render_template('login.html')
+    if request.method == 'GET':
+        return  render_template('login.html')
+    if request.method == 'POST':
+        email = request.form["email"]
+        user =  User.query.filter_by(email = email).first()
+        print(user.email)
+        return redirect(url_for('home'))
+        
+    
 
-@app.route("/signup")
+@app.route("/signup",methods=["GET","POST"])
 def signup():
-    return  render_template('signup.html')
+    if request.method == 'GET':
+        return  render_template('signup.html')
+    if request.method == 'POST':
+        user = User(
+            email=request.form["email"],
+            password=request.form["password"],
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+   
 
 @app.route("/profile")
 def profile():
