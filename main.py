59ac9bd29c8b3ac4f80aecc035c970637a94ba79
Harshmehta14 <harshymehta@gmail.com>
2023-05-14 21:58:20 +0530

    hosting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import Flask, render_template, request, redirect, jsonify, url_for,session
 # from ML_model import *
 from model import *
 import os
@@ -6,28 +6,42 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+os.path.join(os.path.abspath(os.path.dirname(__file__)),"database.sqlite3")
-db.init_app(app)
-with app.app_context():
-    print("database created")
-    db.create_all()
+app.config['SECRET_KEY'] = '1234'
+
+# model = pickle.load(open('model.pkl', 'rb'))
+
 
 
 @app.route("/")
 def home():
-    users =  User.query.all()
-    return  render_template('dashboard.html')
+    # users =  User.query.all()
+    name = ""
+    if 'email' in session:
+        name = session['name']
+    return  render_template('dashboard.html',name=name)
 
 @app.route("/login",methods=["GET","POST"])
 def login():
-    if request.method == 'GET':
-        return  render_template('login.html')
-    if request.method == 'POST':
-        email = request.form["email"]
-        faq =  User.query.filter_by(email = email).first()
-        print(faq.email)
+    status = "Pass"
+    if request.method == "POST":
+        username = request.form['email']
+        password = request.form['password']
+        alert = Login_firebase(username, password)
+        print(alert)
+        if alert == "Failed":
+            status = "Failed"
+            return render_template('login.html', status=status)
+        else:
+            session['email'] = username
+            session['name'] = alert
         return redirect(url_for('home'))
+
+    return render_template('login.html', status=status)
+    # if request.method == 'POST':
+    #     email = request.form["email"]
+    #     faq =  User.query.filter_by(email = email).first()
+    #     print(faq.email)
+    #     return redirect(url_for('home'))
         
     
 
@@ -36,32 +50,41 @@ def signup():
     if request.method == 'GET':
         return  render_template('signup.html')
     if request.method == 'POST':
-        faq = User(
-            email=request.form["email"],
-            password=request.form["password"],
-        )
-        db.session.add(faq)
-        db.session.commit()
+        email=request.form["email"]
+        password=request.form["password"]
+        name=request.form["name"]
+        phno=request.form["phno"]
+        season=request.form["season"]
+        location=request.form["location"]
+
+        data={"email":email,"name":name,"Registered_on": datetime.datetime.now(),"phno":phno,"season":season, "location":location}
+            
+        print(name,email,password,phno,season,location)
+        alert = Register_firebase(data,password)
         return redirect(url_for('login'))
    
 
-@app.route("/profile")
+@app.route("/profile",methods=['GET'])
 def profile():
-    return  render_template('profile.html')
+    data =""
+    if 'email' in session:
+        email = session['email']
+    data = get_user_data(email)
+    return  render_template('profile.html',data=data)
 
 # @app.route('/index')
 # def index():
 #     return render_template('index.html')
 
-# @app.route('/SoilTypeAnalysis',methods=['POST'])
-# def predict():
-#     int_features = [int(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)]
-#     prediction = model.predict(final_features)
+@app.route('/SoilTypeAnalysis',methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
     
-#     output = prediction
+    output = prediction
 
-#     return render_template('SoilTypeAnalysis.html', prediction_text='Suggested crop for given soil health condition is: "{}".'.format(output[0]))
+    return render_template('SoilTypeAnalysis.html', prediction_text='Suggested crop for given soil health condition is: "{}".'.format(output[0]))
 
 # @app.route('/predict_api',methods=['POST'])
 # def predict_api():
@@ -78,21 +101,16 @@ def profile():
 @app.route("/crop_estimation")
 def crop_estimation():
     print("hello")
-    return render_template('./Features/crop_estimation.html')
+    return render_template('./Features/cost_estimation2.html')
 
 @app.route("/crop_comparison")
 def crop_comparison():
     return render_template('./Features/crop_comparison.html')
 
-
-
 @app.route("/faq",methods=["GET","POST"])
 def faq():
     if request.method == 'GET':
-        
-        faq = FAQ.query.all()
-    
-    
+        faq = get_faq()       
         return  render_template('faq.html',faq=faq)
     
 @app.route("/crop_information", methods=["GET"]) 
--- model.py
+++ model.py
@@ -1,20 +1,85 @@
-from flask_sqlalchemy import SQLAlchemy
+from Firebase_config import firebase,auth,db
+import datetime
+import json
 
-db= SQLAlchemy()
+def Register_firebase(data,password):
+    try:
+        # print(username,password,name)
+        user = auth.create_user_with_email_and_password(data["email"],password) 
+        print(user)
+        id= auth.get_account_info(user['idToken'])
+        key = id["users"][0]['localId']
+        if isinstance(key, str): 
+            # data={"username":username,"password":password,"name":name,"Registered_on": datetime.datetime.now(),"id":key,"interests":interests,"address":address}
+            # print(data,key)
+            key_dict = {"id":key}
+            data.update(key_dict)
+            db.collection('users').document(key).set(data)
+        return "Success"
+    except Exception as e:
+        print(e)
+        return "Failed"
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-<<<<<<< HEAD
-   
-    
-=======
-    name = db.Column(db.String, unique=True)
-    phone = db.Column(db.Integer, unique=True)
 
->>>>>>> 6f44713361a01aeb46341165a601786aac42bca6
-class FAQ(db.Model):
-    NO = db.Column(db.Integer, primary_key=True)
-    Questions = db.Column(db.String)
-    Answers = db.Column(db.String)    
+
+def Login_firebase(username,password):
+    try:
+        print(username,password)
+        name=""
+        user = auth.sign_in_with_email_and_password(username,password)
+        id= auth.get_account_info(user['idToken'])
+        key = id["users"][0]['localId']
+        if isinstance(key, str): 
+            data={"last_login": datetime.datetime.now()}
+            db.collection('users').document(key).update(data)
+            n= db.collection('users').where("email","==" ,username).get()[0]
+            name=n.to_dict()['name']
+        return name
+    except Exception as e:
+        print(e)
+        return "Failed"
+
+def get_faq():
+    faq = db.collection('faq').get()
+    q_a = []
+    for doc in faq:
+        q_a.append(doc.to_dict())
+    print(q_a)
+    return q_a
+
+def get_user_data(email):
+    user = db.collection('users').where("email","==" ,email).get()
+    for doc in user:
+        user_data = doc.to_dict()
+
+    return user_data
+#     rewards = a.to_dict()['rewards']
+#     return rewards
+
+# a= Login_firebase("test@abc.coaa","12345") 
+# print(a)
+# print(type(id["users"][0]['localId']))
+# a= db.collection('users').where("name","==" ,"test").get()[0].id
+# print(a)
+
+# def get_rewards_firebase(username):
+#     a = db.collection('users').where("username","==" ,username).get()[0]
+#     rewards = a.to_dict()['rewards']
+#     return rewards
+
+# def add_rewards_firebase(username,amt,action):
+#     try:
+#         a = db.collection('users').where("username","==" ,username).get()[0]
+#         key=a.to_dict()['id']
+#         rewards = a.to_dict()['rewards']
+#         db.collection('users').document(key).update({"rewards":rewards+amt})
+#         return "Updated"
+#     except Exception as e:
+#         print(e)
+#         return "Failed"
+
+# def get_user_info(username):
+#     user_info = db.collection('users').where("username","==" ,username).get()[0]
+#     return user_info.to_dict()
+
+
